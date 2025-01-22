7d79fd99d8558df81dba573af60bc551212b73bf
samsul7718 <samsul7718@gmail.com>
2025-01-22 17:17:45 +0530

    first commit

---
+++ Easy/src/Basic/Amstrong.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Amstrong {
+    public static void main(String[] args) {
+        int n,c,r,rem,arm=0;
+        System.out.print("Enter any number");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        c=n;
+
+        while(n>0){
+          rem=n%10;
+          arm=(rem*rem*rem)+arm;
+          n=n/10;
+
+        }
+        if(c==arm){
+            System.out.println("Amstrong Number");
+        }else{
+            System.out.println("Not Amstrong number");
+        }
+    }
+}
